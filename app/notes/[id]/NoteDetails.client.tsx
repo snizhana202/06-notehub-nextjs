f05c85720a78822ab61e1c57f7aa578e44a23562
Snizhana <snizhanapetrushka@gmail.com>
2026-03-29 16:50:24 +0300

    hw

---
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -0,0 +1,39 @@
+// app/notes/[id]/NoteDetails.client.tsx
+
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from 'next/navigation';
+import { fetchNoteById } from "@/lib/api";
+import css from "./NoteDetails.module.css";
+
+const NoteDetailsClient = () => {
+	const params = useParams();
+  const id = params.id as string;
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error || !data?.note) return <p>Some error..</p>;
+ const note = data.note;
+
+  return (
+    <div className={css.container}>
+      <div className={css.item}>
+        <div className={css.header}>
+          <h2>{note.title}</h2>
+        </div>
+        <p className={css.tag}>{note.tag}</p>
+        <p className={css.content}>{note.content}</p>
+        <p className={css.date}>{note.createdAt}</p>
+      </div>
+    </div>
+  );
+};
+
+export default NoteDetailsClient;
